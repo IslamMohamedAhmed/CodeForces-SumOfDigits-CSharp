274d8103dcf7f3961c02d485ddaf62f52c57b370
unknown <islammohamedahmed49@gmail.com>
2024-04-13 13:10:38 +0200

    sod

---
+++ SumOfDigits/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+using static System.Convert;
+using static System.Math;
+
+
+string str;
+int counter = 0;
+
+
+str = ReadLine();
+
+
+while(str.Length != 1)
+{
+    long sum = 0;
+    for(int i=0;i<str.Length; i++)
+    {
+        sum += str[i] - '0';
+    }
+    str = $"{sum}";
+    counter++;
+}
+
+WriteLine(counter);
